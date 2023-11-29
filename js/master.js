3c0e986c4be62dcce0cedd839f1af589921bc045
Ahmed safwat <a01273477051@gmail.com>
2023-11-29 07:17:38 +0200

    made a function to switch the active class on the titles

--- js/master.js
+++ js/master.js
@@ -1,10 +1,14 @@
 // local storage check{
 let mainColor = localStorage.getItem("color-option");
 if (mainColor != null) {
-  document.documentElement.style.setProperty(
-    "--primary-color",
-    localStorage.getItem("color-option")
-  );
+  document.documentElement.style.setProperty("--primary-color", mainColor);
+  // check for active class
+  document.querySelectorAll(".colors-list li").forEach((ele) => {
+    ele.classList.remove("active");
+    if (ele.dataset.color === mainColor) {
+      ele.classList.add("active");
+    }
+  });
 }
 // toggle class on click
 document.querySelector(".toggle-settings").onclick = function () {
@@ -29,6 +33,49 @@ colorsLi.forEach((ele) => {
   });
 });
 
+// switch background options
+const randomBgElement = document.querySelectorAll(".random-backgrounds span");
+// random background option
+let backgroundOption = true;
+
+// variable to control the interval
+let bgInterval;
+
+// chick if there is storage random background item
+let backgroundLocalItem = localStorage.getItem("background-option");
+if (backgroundLocalItem != null) {
+  randomBgElement.forEach((element) => {
+    element.classList.remove("active");
+  });
+  if (backgroundLocalItem === "true") {
+    backgroundOption = true;
+    document.querySelector(".random-backgrounds .yes").classList.add("active");
+  } else {
+    backgroundOption = false;
+    document.querySelector(".random-backgrounds .no").classList.add("active");
+  }
+}
+
+//loop on all spans
+randomBgElement.forEach((span) => {
+  span.addEventListener("click", (e) => {
+    e.target.parentElement.querySelectorAll(".active").forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    if (e.target.dataset.background == "yes") {
+      backgroundOption = true;
+      randomizeBackground();
+      localStorage.setItem("background-option", true);
+    } else {
+      backgroundOption = false;
+      clearInterval(bgInterval);
+      localStorage.setItem("background-option", false);
+    }
+  });
+});
+
+// random background
 const landingPage = document.querySelector(".landing-page");
 
 // get array of images
@@ -42,10 +89,27 @@ imgsArray.forEach((imgUrl) => {
   imgObjects.push(img);
 });
 
-// change background image url
-setInterval(() => {
-  let randomNumber = Math.floor(Math.random() * imgObjects.length);
-  landingPage.style.transition =
-    "background-image 1.2s cubic-bezier(0.4, 0, 1, 1) 0s";
-  landingPage.style.backgroundImage = `url('${imgObjects[randomNumber].src}')`;
-}, 10000);
+// function to randomize background
+function randomizeBackground() {
+  if (backgroundOption === true) {
+    // change background image url
+    bgInterval = setInterval(() => {
+      let randomNumber = Math.floor(Math.random() * imgObjects.length);
+      landingPage.style.transition =
+        "background-image .6s cubic-bezier(0.4, 0, 1, 1) 0s";
+      landingPage.style.backgroundImage = `url('${imgObjects[randomNumber].src}')`;
+    }, 10000);
+  }
+}
+randomizeBackground();
+const activeNavbar = document.querySelectorAll("ul li a");
+
+activeNavbar.forEach((ele) => {
+  ele.addEventListener("click", (e) => {
+    // Remove "active" class from all links
+    activeNavbar.forEach((link) => link.classList.remove("active"));
+
+    // Add "active" class to the clicked link
+    e.target.classList.add("active");
+  });
+});
